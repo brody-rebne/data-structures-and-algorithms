2fd155085999118ef3e741716dc9fe8d72fa4310
brody-rebne <bsrebne@gmail.com>
2020-02-11 17:29:20 -0800

    completed challenges

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -9,7 +9,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
+  const endString = `${str} The end.`;
+  return endString;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -26,7 +27,7 @@ console.log(a) prints [1, 2, 3, 1]
 ------------------------------------------------------------------------------------------------ */
 
 const appendFirstToLast = (arr) => {
-  // Solution code here...
+  arr.push(arr[0]);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -45,7 +46,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
 const addBirthYearProperty = (obj, year) => {
-  // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -62,7 +63,9 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  // Solution code here...
+  people.forEach(person => {
+    person.isAuthor = true;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -81,8 +84,10 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
-
+  const arr2Length = arr2.length;
+  for(let i=0; i<arr2Length; i++) {
+    arr1.push(arr2[i]);
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------

2b7f1029219501390f2dcfb30d1234de960671e6
brody-rebne <brody-rebne@BRODYDESK>
2020-03-29 08:45:23 -0700

    file setup

---
+++ code-challenges/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class Queue
+    {
+        public Node Front { get; set; }
+
+        /// <summary>
+        /// Adds a new node to the back of the queue
+        /// </summary>
+        /// <param name="val">The value of the node to be added</param>
+        public void Enqueue(int val)
+        {
+
+        }
+
+        /// <summary>
+        /// Removes a node from the front of the queue
+        /// </summary>
+        public void Dequeue()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns the value of the node at the front of the queue
+        /// </summary>
+        /// <returns>The value of the node at the front of the queue</returns>
+        public int Peek()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Checks if the queue is empty
+        /// </summary>
+        /// <returns>True if the queue is empty</returns>
+        public bool IsEmpty()
+        {
+            return false;
+        }
+    }
+}
---
+++ code-challenges/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+
+        /// <summary>
+        /// Pushes a node on the top of the stack
+        /// </summary>
+        /// <param name="val">The value of the node to be added</param>
+        public void Push(int val)
+        {
+
+        }
+
+        /// <summary>
+        /// Pops the top node off of the stack
+        /// </summary>
+        public void Pop()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns the value of the node on top of the stack
+        /// </summary>
+        /// <returns>The value of the node at the top of the stack</returns>
+        public int Peek()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Checks if the stack is empty
+        /// </summary>
+        /// <returns>True if stack is empty</returns>
+        public bool IsEmpty()
+        {
+            return false;
+        }
+    }
+}

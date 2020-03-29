4d91149fe1e35cf2851f42a6bdb522d157b9c29e
brody-rebne <brody-rebne@BRODYDESK>
2020-03-29 10:03:42 -0700

    can push multiple nodes to stack

--- code-challenges/StacksAndQueues/StacksAndQueuesTests/UnitTest1.cs
+++ code-challenges/StacksAndQueues/StacksAndQueuesTests/UnitTest1.cs
@@ -16,5 +16,18 @@ namespace StacksAndQueuesTests
 
             Assert.Equal("Top -> 12 -> null", stackString);
         }
+
+        //test that multiple values can be pushed onto the stack
+        [Fact]
+        public void CanPushMultipleToStack()
+        {
+            Stack stack = new Stack();
+            stack.Push(7);
+            stack.Push(8);
+            stack.Push(9);
+            string stackString = stack.ToString();
+
+            Assert.Equal("Top -> 9 -> 8 -> 7 -> null", stackString);
+        }
     }
 }
